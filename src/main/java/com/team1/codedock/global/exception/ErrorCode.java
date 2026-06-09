0be4147b6444821f914efa06184fc3a8a8103a90
Jinhyen Kim <jin@Jinhyens-MacBook-Air.local>
2026-06-09 12:18:01 +0900

    feat: 중복 사용자명 에러 코드 추가

--- src/main/java/com/team1/codedock/global/exception/ErrorCode.java
+++ src/main/java/com/team1/codedock/global/exception/ErrorCode.java
@@ -19,6 +19,7 @@ public enum ErrorCode {
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "A001", "유효하지 않은 토큰입니다."),
     EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "A002", "만료된 토큰입니다."),
     EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT, "A003", "이미 사용 중인 이메일입니다."),
+    USERNAME_ALREADY_EXISTS(HttpStatus.CONFLICT, "A004", "이미 사용 중인 사용자명입니다."),
 
     // User
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "U001", "사용자를 찾을 수 없습니다."),
